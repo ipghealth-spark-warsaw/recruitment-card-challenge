575644513ff0d37cf86a67b009d1a9c4ecf14385
biodiscus <biodiscus@gmail.com>
2024-10-11 18:46:59 +0200

    chore: optimize webpack config for pug-plugin

--- js/images.js
+++ js/images.js
@@ -1,4 +1,4 @@
-import '../images/background.jpg'
-import '../images/chip.png'
-import '../images/visa.png'
-import '../images/mastercard.png'
+//import '../images/background.jpg'
+//import '../images/chip.png'
+//import '../images/visa.png'
+//import '../images/mastercard.png'
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,11 @@
 const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const PugPlugin = require("pug-plugin");
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const ErrorNotificationPlugin = require("webpack-error-notification-plugin");
 
-
-
 const devMode = process.env.NODE_ENV !== "production";
-let slides = [];
-slides.push(addSlide('index'))
-
+const isProduction = process.env.NODE_ENV === "production";
 
 const browserSync = new BrowserSyncPlugin({
     host: "localhost",
@@ -18,21 +13,37 @@ const browserSync = new BrowserSyncPlugin({
     proxy: "http://localhost:8080/",
 });
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const miniCss = new MiniCssExtractPlugin({
-    filename: "css/[name].css",
-});
-
 const moduleReplacement = new webpack.HotModuleReplacementPlugin();
 const noEmit = new webpack.NoEmitOnErrorsPlugin();
 
-module.exports = {
-    entry: [`${__dirname}/js/main.js`, `${__dirname}/scss/app.scss`],
+const pages = new PugPlugin({
+    entry: [
+      // add pages here
+      {
+        import: 'views/index.pug', // Pug template
+        filename: 'index.html', // output HTML into dist/index.html
+      },
+    ],
+    js: {
+      filename: 'js/[name].[contenthash:8].js', // JS output filename
+    },
+    css: {
+      filename: 'css/[name].[contenthash:8].css', // CSS output filename
+    },
+});
 
+module.exports = {
     output: {
         path: `${__dirname}/dist`,
-        filename: "js/main.js",
+    },
+
+    resolve: {
+        // aliases used in SCSS and in templates
+        alias: {
+            "@images": `${__dirname}/images`,
+            "@styles": `${__dirname}/scss`,
+            "@scripts": `${__dirname}/js`,
+        },
     },
 
     devServer: {
@@ -60,18 +71,8 @@ module.exports = {
                 test: /\.scss$/,
                 use: [
                     {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: "../",
-                        },
-                    },
-
-                    {
                         loader: "css-loader",
                         options: { sourceMap: devMode, url: true },
-                    },
-                    {
-                        loader: 'resolve-url-loader'
                     },
                     {
                         loader: "sass-loader",
@@ -80,25 +81,11 @@ module.exports = {
                 ],
             },
             {
-                test: /\.pug$/,
-                use: {
-                    loader: "@webdiscus/pug-loader",
-                },
-            },
-            {
                 test: /\.(jp?g|png|gif|svg)$/i,
-                // type: 'asset/resource',
-                // generator: {
-                //     filename: 'images/[name][ext]'
-                // },
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "images/[name].[ext]",
-                        },
-                    }
-                ],
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                },
             },
         ],
     },
@@ -107,19 +94,8 @@ module.exports = {
         new CleanWebpackPlugin(),
         new ErrorNotificationPlugin(),
         browserSync,
-        miniCss,
-        ...slides,
+        pages,
         noEmit,
         moduleReplacement,
-
     ],
 };
-
-function addSlide(name) {
-    return new HtmlWebpackPlugin({
-        template: `views/${name}.pug`,
-        inject: "body",
-        mobile: true,
-        filename: `${name}.html`,
-    });
-}
